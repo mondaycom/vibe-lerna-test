b66179cc0b1217ea7566e953f77665e9cc5b7adf
Sergey Roytman <sergeyro@monday.com>
2023-12-18 11:43:36 +0200

    Delete everything except for .gitignore

--- src/storybook/components/index.js
+++
@@ -1,5 +0,0 @@
-import { ComponentNameDecorator } from "./component-name/component-name-decorator";
-import { RelatedComponentsDecorator } from "./related-components/related-components-decorator";
-import { ComingSoon } from "./coming-soon/coming-soon";
-
-export { ComponentNameDecorator, RelatedComponentsDecorator, ComingSoon };
