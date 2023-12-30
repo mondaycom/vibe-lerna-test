0bb71070c2045aaaf971c77b15dd60ec6375ce35
Sergey Roytman <sergeyro@monday.com>
2023-12-30 18:09:16 +0200

    ci: prerelease workflow

---
+++ scripts/get-version-preid.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+function getVersionPreid() {
+  const branchName = process.env.BRANCH_NAME;
+  const commitSHA = process.env.COMMIT_SHA;
+
+  // Find the last occurrence of the '/' character
+  const index = branchName.lastIndexOf("/");
+  let finalPrName = branchName.replaceAll("_", "-");
+  // If the character was found, return the substring after it
+  if (index !== -1) {
+    finalPrName = branchName.substring(index + 1);
+  }
+
+  return `${finalPrName}-${commitSHA.substring(0, 10)}`;
+}
+
+getVersionPreid();
