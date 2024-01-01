e68cdae260cb0d906ef9e24b3ef6668c3590b7e5
Sergey Roytman <sergeyro@monday.com>
2024-01-01 22:57:14 +0200

    devug output [prerelease]

--- scripts/get-packages-versions-diff.js
+++ scripts/get-packages-versions-diff.js
@@ -7,21 +7,32 @@ function getPackagesVersionsDiff() {
   const package_version_before_release = process.env.package_version_before_release;
   const package_version_after_release = process.env.package_version_after_release;
 
+  console.log("### getPackagesVersionsDiff");
+  console.log("### getPackagesVersionsDiff package_version_before_release", package_version_before_release);
+  console.log("### getPackagesVersionsDiff package_version_after_release", package_version_after_release);
+
   const fs = require("fs");
   const path = require("path");
   const PACKAGES_PATH = path.join(__dirname, "../packages");
 
-  const packagesVersionsDiff = {};
+  const packages_versions_diff = {};
 
   fs.readdirSync(PACKAGES_PATH).forEach(packageName => {
+    console.log("### getPackagesVersionsDiff packageName", packageName);
+    console.log(
+      "### getPackagesVersionsDiff package_version_before_release[packageName]",
+      package_version_before_release[packageName],
+    );
     if (package_version_before_release[packageName] !== package_version_after_release[packageName]) {
-      packagesVersionsDiff[packageName] = package_version_after_release[packageName];
+      packages_versions_diff[packageName] = package_version_after_release[packageName];
     }
   });
 
-  console.log(packagesVersionsDiff);
-  fs.writeFileSync(process.env.GITHUB_OUTPUT, `packages_versions_diff=${packagesVersionsDiff}`);
-  return packagesVersionsDiff;
+  console.log(packages_versions_diff);
+  console.log("### getPackagesVersionsDiff packages_versions_diff", packages_versions_diff);
+
+  fs.writeFileSync(process.env.GITHUB_OUTPUT, `packages_versions_diff=${packages_versions_diff}`);
+  return packages_versions_diff;
 }
 
 getPackagesVersionsDiff();
