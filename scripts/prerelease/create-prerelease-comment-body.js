99e831bd3d956573aea4c6d4edfcd726f2071c5f
Sergey Roytman <sergeyro@monday.com>
2024-01-02 14:35:18 +0200

    add create-prerelease-comment-body.js [prerelease]

---
+++ scripts/prerelease/create-prerelease-comment-body.js
@@ -0,0 +1,22 @@
+/**
+ * Create a comment body for a PR that contains a prerelease version of monorepo packages.
+ */
+function createPrereleaseCommentBody() {
+  const fs = require("fs");
+
+  const packages_versions_diff = JSON.parse(process.env.PACKAGES_VERSIONS_DIFF);
+  let comment_body = "";
+  for (const package_name in packages_versions_diff) {
+    const new_version = packages_versions_diff[package_name];
+    comment_body += `A new prerelease version of this PR has been published: \`${package_name}@${new_version}\`\n`;
+    comment_body +=
+      "To install this prerelease version using npm, please run the following command in your terminal:\n";
+    comment_body += `\`npm i ${package_name}@${new_version}\`\n`;
+    comment_body += "If you are using Yarn, you can use the following command instead:\n";
+    comment_body += `\`yarn add ${package_name}@${new_version}\`\n\n`;
+  }
+
+  fs.writeFileSync(process.env.GITHUB_OUTPUT, `comment_body=${comment_body}`);
+}
+
+createPrereleaseCommentBody();
