6e23fbd7e482eff631d9ec69ff76e8bb12dff6f2
Sergey Roytman <sergeyro@monday.com>
2024-01-02 14:59:11 +0200

    add debug output - create-prerelease-comment-body.js [prerelease]

--- scripts/prerelease/create-prerelease-comment-body.js
+++ scripts/prerelease/create-prerelease-comment-body.js
@@ -1,21 +1,27 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+
 /**
  * Create a comment body for a PR that contains a prerelease version of monorepo packages.
  */
 function createPrereleaseCommentBody() {
-  const fs = require("fs");
-
   const packages_versions_diff = JSON.parse(process.env.PACKAGES_VERSIONS_DIFF);
+  console.log("### createPrereleaseCommentBody packages_versions_diff", packages_versions_diff);
   let comment_body = "";
   for (const package_name in packages_versions_diff) {
+    console.log("### createPrereleaseCommentBody package_name", package_name);
     const new_version = packages_versions_diff[package_name];
+    console.log("### createPrereleaseCommentBody new_version", new_version);
     comment_body += `A new prerelease version of this PR has been published: \`${package_name}@${new_version}\`\n`;
-    comment_body +=
-      "To install this prerelease version using npm, please run the following command in your terminal:\n";
+    comment_body += `To install this prerelease version using npm, please run the following command in your terminal:\n`;
     comment_body += `\`npm i ${package_name}@${new_version}\`\n`;
-    comment_body += "If you are using Yarn, you can use the following command instead:\n";
+    comment_body += `If you are using Yarn, you can use the following command instead:\n`;
     comment_body += `\`yarn add ${package_name}@${new_version}\`\n\n`;
+    console.log("### createPrereleaseCommentBody +package_name comment_body", package_name, comment_body);
   }
 
+  console.log("### createPrereleaseCommentBody before writing down the result comment_body", comment_body);
   fs.writeFileSync(process.env.GITHUB_OUTPUT, `comment_body=${comment_body}`);
 }
 
